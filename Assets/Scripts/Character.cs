6d93a1f94788db8a7f51982c7eabe8ea7e06e81e
Brent Chance <solarfroggames@gmail.com>
2020-04-18 16:06:17 -0700

    Separated speed into speedMove and speedAnim.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,7 +6,8 @@ public class Character : MonoBehaviour
 {
 	public Vector3 target;
 	public float size = 1.0f;
-	public float speed = 2.0f;
+	public float speedMove = 1.0f;
+	public float speedAnim = 1.0f;
 
 	private Animator _animator;
 	private bool _walking = false;
@@ -18,10 +19,10 @@ public class Character : MonoBehaviour
 	
     void Update()
     {
-		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+		transform.position = Vector3.MoveTowards(transform.position, target, speedMove * Time.deltaTime);
 
 		_walking = Vector2.Distance(transform.position, target) > 0.1f;
 		_animator.SetBool("Walking", _walking);
-		_animator.speed = speed;
+		_animator.speed = speedAnim;
     }
 }
