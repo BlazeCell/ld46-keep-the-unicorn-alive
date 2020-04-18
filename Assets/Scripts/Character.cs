8c2942c4efa4b936490a117150ba441a6d59cb1a
Shane Smith <ShanePatSmith@gmail.com>
2020-04-17 23:17:43 -0400

    Added a comment to public float size

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-	public float size = 1;
+	public float size = 1;  //Shane meddling with your code
 	public Vector3 target;
 
     // Start is called before the first frame update
