e11928fb51d41b1504067ee2772db1b2de46c7ce
Shane Smith <ShanePatSmith@gmail.com>
2020-04-17 23:48:34 -0400

    Added the basis for the bush script

---
+++ Assets/Scripts/Bush.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	public bool trap;
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider2D other)
+    {
+        if (trap == true)
+        {
+            Debug.Log("There was a trap!");
+        }
+        else
+        {
+            Debug.Log("There was no trap.");
+        }
+    }
+
+}
