31858d6c4a4bbecccfbb687c87b9920b20cc2934
Brent Chance <solarfroggames@gmail.com>
2020-04-17 21:17:10 -0700

    Character walk animation works

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-	public float size = 1;  //Shane meddling with your code
 	public Vector3 target;
+	public float size = 1.0f;
+	public float speed = 2.0f;
 
-    // Start is called before the first frame update
+	private Animator _animator;
+	private bool _walking = false;
+	
     void Start()
     {
-        
+		_animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+	
     void Update()
     {
-        
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+		_walking = Vector2.Distance(transform.position, target) > 0.1f;
+		_animator.SetBool("Walking", _walking);
     }
 }
