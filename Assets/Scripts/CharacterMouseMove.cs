0e8fccf3c3884855d781cd800da085d153bbfa42
Brent Chance <solarfroggames@gmail.com>
2020-04-18 14:15:36 -0700

    Player movement via mouse implemented.

---
+++ Assets/Scripts/CharacterMouseMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMouseMove : MonoBehaviour
+{
+	private Character _character;
+
+    void Start()
+    {
+		_character = GetComponent<Character>();
+    }
+	
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+		{
+			var mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			mouseWorld.z = 0.0f;
+			_character.target = mouseWorld;
+		}
+    }
+}
